c39cfcd3eeabda58112c617e6f1a0dd9f4b23352
tharle <tharles.josefidecamargo@gmail.com>
2021-01-30 23:51:19 -0500

    Class 77

---
+++ Assets/Scripts/events/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Classe que gerencia os eventos de portas
+/// </summary>
+public class DoorController : MonoBehaviour {
+
+    [Header("Scripts externos")]
+    private PlayerScript playerScript;
+    
+    [Header("Configuração")]
+    public Transform destino;
+
+    void Start() {
+        this.playerScript = FindObjectOfType(typeof(PlayerScript)) as PlayerScript;
+    }
+
+    public void Interacao() {
+        print("O jogador abriu a porta");
+        playerScript.transform.position = destino.position;
+
+    }
+}
